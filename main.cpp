18954fa5dc5f9984e94cab5ca49514f5570f8982
Albert Manankov <modwert404@gmail.com>
2026-02-28 13:49:11 +0700

    level 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <functional>
 
-int main()
+
+int doWork()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout<<std::this_thread::get_id() << " doWork started"<<std::endl;
+    std::this_thread::sleep_for(3 * std::chrono::seconds(1));
+    std::cout<<std::this_thread::get_id() << " doWork finished\n"<<std::endl;
+    return 0;
+}
+
+int main(){
+    std::thread t1(doWork);
+
+    //t1.detach();//finish second thread if main thread is finished
+
+    //t1.join();//just wait when this thread finished and after go ahead
+
+    std::cout<<std::this_thread::get_id() << " main started"<<std::endl;
+    std::this_thread::sleep_for(3 * std::chrono::seconds(1));
+    std::cout<<std::this_thread::get_id() << " main finished"<<std::endl;
     return 0;
 }
