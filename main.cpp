026a1e5e2191f1afaae44bc13c703c89f7553389
Albert Manankov <modwert404@gmail.com>
2026-03-06 22:31:54 +0700

    level 2

--- main.cpp
+++ main.cpp
@@ -4,23 +4,37 @@
 #include <functional>
 
 
-int doWork()
+void doWork(const int a, const int b,int &c)
 {
-    std::cout<<std::this_thread::get_id() << " doWork started"<<std::endl;
-    std::this_thread::sleep_for(3 * std::chrono::seconds(1));
-    std::cout<<std::this_thread::get_id() << " doWork finished\n"<<std::endl;
-    return 0;
+    std::this_thread::sleep_for(2 * std::chrono::seconds(1));
+    c = a + b;
 }
 
 int main(){
-    std::thread t1(doWork);
+    //auto result = new int(0);
+    // std::thread t1(doWork,2,2,*result);
+
+    int result;
+
+    std::thread t1(doWork,9,1,std::ref(result));
 
     //t1.detach();//finish second thread if main thread is finished
 
     //t1.join();//just wait when this thread finished and after go ahead
 
-    std::cout<<std::this_thread::get_id() << " main started"<<std::endl;
-    std::this_thread::sleep_for(3 * std::chrono::seconds(1));
-    std::cout<<std::this_thread::get_id() << " main finished"<<std::endl;
+    // while(true)
+    // {
+    //     std::this_thread::sleep_for(3 * std::chrono::seconds(1));
+    // }
+    // std::cout<<std::this_thread::get_id() << " main started"<<std::endl;
+    // std::this_thread::sleep_for(3 * std::chrono::seconds(1));
+    // std::cout<<std::this_thread::get_id() << " main finished"<<std::endl;
+
+    t1.join();
+    std::cout << result << std::endl;
+
+    // delete result;
+
     return 0;
+
 }
