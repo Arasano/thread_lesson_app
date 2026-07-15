bf7af50428e61c0daf78bb78d3fed30b5db3553c
𝓐𝓻𝓪𝓼𝓪𝓷𝓸 <106920436+defwert404@users.noreply.github.com>
2026-07-15 18:56:36 +0700

    level 9

--- main.cpp
+++ main.cpp
@@ -1,31 +1,52 @@
 #include <chrono>
 #include <functional>
 #include <iostream>
+#include <list>
 #include <mutex>
 #include <thread>
 
-void log(const int num) { std::cout << num << " \n"; }
-
-void doWork(const int num, std::recursive_mutex &mt1) {
-    std::lock_guard<std::recursive_mutex> lock_guard(mt1);
-    // mt1.lock();
-    if (num < 0) {
-        std::cout << std::endl;
-        mt1.unlock();
-        return;
+void log(const std::list<int> &list) {
+    std::cout << "\nlist: [ ";
+    for (int i : list) {
+        std::cout << i << ", ";
     }
-    log(num);
-    std::this_thread::sleep_for(1 * std::chrono::milliseconds(200));
-    doWork(num - 1, mt1);
-    // mt1.unlock();
+    std::cout << " ]\n" << std::endl;
+}
+
+void doWork1(std::list<int> &list, bool is_minus, std::mutex &mt1) {
+    std::unique_lock<std::mutex> unique_lock_1(
+        mt1,
+        std::defer_lock);  // usual unique_lock    std::unique_lock<std::mutex>
+                           // unique_lock_1(mt1); with lock at the begining
+    // asynchronous
+    std::this_thread::sleep_for(5 * std::chrono::seconds(1));
+
+    // synchronous
+    unique_lock_1.lock();
+
+    int q = 1;
+    if (is_minus) {
+        q = -1;
+    }
+    int count = 0;
+    while (count < 100) {
+        count += 2;
+        list.push_back(count * q);
+        log(list);
+    }
+
+    unique_lock_1.unlock();
+
+    // asynchronous
+    std::this_thread::sleep_for(5 * std::chrono::seconds(1));
 }
 
 int main() {
-    std::recursive_mutex mt1;
+    std::mutex mt;
+    std::list<int> list_1;
 
-    std::thread t1(doWork, 10, std::ref(mt1));
-    std::this_thread::sleep_for(1 * std::chrono::milliseconds(10));
-    std::thread t2(doWork, 30, std::ref(mt1));
+    std::thread t1(doWork1, std::ref(list_1), false, std::ref(mt));
+    std::thread t2(doWork1, std::ref(list_1), true, std::ref(mt));
     t1.join();
     t2.join();
     return 0;
